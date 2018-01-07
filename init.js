f75b50da58ff02e383ba2bd8a720a2c45eb8d554
Andrey Reinwald <andreinwald@gmail.com>
2018-01-07 22:06:32 +0200

    Fix path

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-import Config from '/Users/andreinwald/www/real2/realmetric2.github.io/config.js'
+import Config from '/config.js'
 import Router from '/Library/Router.js'
 
 let router = new Router(Config.routes);
